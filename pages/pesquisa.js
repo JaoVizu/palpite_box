83131c982b1ff03cfea453d71f5ce932cd527bc0
Joao Vizu <joaovizu2015@gmail.com>
2020-07-11 13:19:19 -0300

    Readme do projeto

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -45,7 +45,8 @@ const Pesquisa = () => {
       <p className="text-center mb-6">
         O restaurante X sempre busca por atender melhor seus clientes. <br />
         Por isso, estamos sempre abertos a ouvir sua opinião.</p>
-      {!success && <div className="w-1/5 mx-auto">
+      {!success && 
+      <div className="w-1/5 mx-auto">
         <label className="font-bold">Seu nome:</label>
         <input name="" id="" type="text" value="" 
           className="p-4 block shadow rounded bg-blue-200 my-2 w-full"
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Palpite Box
+
+## Projeto criado durante a semana FSM ministrada por Tulio Faria.
+
+
+Palpite box é um sistema de crítica ou sugestão de um restaurante. O cliente acessa o site, dá sua opinião e pode ganhar um cupom caso o restaurante tenha ativado essa opção.
+
+Todo o gerenciamento é feito por uma planilha do Google.
+
+### Tecnologias
+
+* JavaScript
+* ReactJS
+* NextJS
+
+# Utilizando o projeto
+
+#### Instalando as dependências
+`npm install`
+
+#### Inciar o projeto
+`npm run dev`
+
